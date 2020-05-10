0a428333994040ed0e0049d5b4df820daa5da5f3
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-05-10 03:12:25 -0300

    update scripts

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,4 @@
-var cardDeck = document.querySelector("div");
+var app = document.getElementById("app");
 
 var places = fetch("https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72")
   .then((res) => res.json())
@@ -45,6 +45,6 @@ var places = fetch("https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72")
       cardList.appendChild(cardBody);
       cardList.appendChild(cardFooter);
 
-      cardDeck.appendChild(cardList);
+      app.appendChild(cardList);
     }
   });
